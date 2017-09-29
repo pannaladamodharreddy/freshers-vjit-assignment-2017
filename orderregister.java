8e07f72a5f6e9edd8f261a6843a7b7dbc22c1c7d
Pannala Damodhar Reddy <32167301+pannaladamodharreddy@users.noreply.github.com>
2017-09-29 21:01:43 +0530

    Add files via upload
    >To maintain order register
    >To record the number of customers who had ordered their food and to maintain their record.

---
+++ orderregister.java
@@ -0,0 +1,12 @@
+scanner input=new scanner(system.in);
+double order=0.0;
+string customers;
+int ordercounter=0;
+while(price!=0)
+if(customercounter!=0)
+{
+system.out.println("enter if any customer had ordered");
+order=input.nextdouble();
+ordercounter=order+1;
+system.out.println("the number of orders which are registered in order register is:"+ordercounter);
+}
